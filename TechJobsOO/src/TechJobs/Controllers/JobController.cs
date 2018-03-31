29a547be21225fa04566dd518ce20c834c082563
asjoseph <34357308+asjoseph@users.noreply.github.com>
2018-03-31 11:21:30 -0400

    Add files via upload
    Nav bar only shows all three links when in full screen, in order to see "New"

---
+++ TechJobsOO/src/TechJobs/Controllers/JobController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TechJobs.Data;
+using TechJobs.ViewModels;
+using TechJobs.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TechJobs.Controllers
+{
+    public class JobController : Controller
+    {
+
+        // Our reference to the data store
+        private static JobData jobData;
+
+        static JobController()
+        {
+            jobData = JobData.GetInstance();
+        }
+
+        // The detail display for a given Job at URLs like /Job?id=17
+        public IActionResult Index(int id)
+        {
+            // TODO #1 - get the Job with the given ID and pass it into the view
+            jobData = JobData.GetInstance();
+            Job job = jobData.Find(id);
+
+            return View(job);
+        }
+
+        public IActionResult New()
+        {
+            NewJobViewModel newJobViewModel = new NewJobViewModel();
+            return View(newJobViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult New(NewJobViewModel newJobViewModel)
+        {
+            // TODO #6 - Validate the ViewModel and if valid, create a 
+            // new Job and add it to the JobData data store. Then
+            // redirect to the Job detail (Index) action/view for the new Job.
+            if (ModelState.IsValid)
+            {
+                Job newJob = new Job()
+                {
+                    Name = newJobViewModel.Name,
+                    Employer = jobData.Employers.Find(newJobViewModel.EmployerID),
+                    CoreCompetency = jobData.CoreCompetencies.Find(newJobViewModel.CoreCompetencyID),
+                    Location = jobData.Locations.Find(newJobViewModel.LocationID),
+                    PositionType = jobData.PositionTypes.Find(newJobViewModel.PositionTypeID)
+                };
+
+                jobData.Jobs.Add(newJob);
+
+                return Redirect("/Job?id=" + newJob.ID.ToString());
+            }
+            return View(newJobViewModel);
+        }
+    }
+}
